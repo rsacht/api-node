5e4647b3b36ec46849c67aafecf91019d141f123
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-17 19:41:53 -0300

    Criando a rota POST de produtos

--- src/app.js
+++ src/app.js
@@ -12,8 +12,13 @@ const route = router.get('/', (req, res, next) =>{
         version: "0.0.1"
     });
 });
+
+const create = router.post('/', (req, res, next) =>{
+    res.status(200).send(req.body);
+});
 //Atribuimos a Rota no app
 app.use('/', route);
+app.use('/products', create);
 
 //Exportando o app
 module.exports = app;

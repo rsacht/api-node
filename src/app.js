60b0392fad63b344c11acf44a4b80ee8cbd72fa9
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-17 20:43:34 -0300

    Finalizando PUT

--- src/app.js
+++ src/app.js
@@ -21,13 +21,17 @@ const route = router.get('/', (req, res, next) =>{
 const create = router.post('/', (req, res, next) =>{
     res.status(200).send(req.body);
 });
-const create = router.put('/:id', (req, res, next) =>{
+const put = router.put('/:id', (req, res, next) =>{
     const id = req.params.id;
-    res.status(201).send(req.body);
+    res.status(201).send({
+        id:id,
+        item: req.body
+    });
 });
 //Atribuimos a Rota no app
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
 
 //Exportando o app
 module.exports = app;

3e630355b14e1eb979502bc14a7d6bd22701b9ca
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-17 20:19:24 -0300

    A extendedopção permite escolher entre a análise dos dados codificados por URL com a querystringbiblioteca (quando false) ou a qsbiblioteca (quando true). A sintaxe estendida permite que objetos e matrizes sofisticados sejam codificados no formato codificado por URL, permitindo uma experiência semelhante a JSON com codificação de URL. Para mais informações, consulte a biblioteca qs .

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 const router = express.Router();
 

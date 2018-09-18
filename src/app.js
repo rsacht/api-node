22a08f1bdff1107cbf40cf18faa2118e0fac7b56
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-18 02:00:08 -0300

    Conectando e Autenticando com o banco de dados

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
 const router = express.Router();
 
-const app = express();
+//Conexão com o Banco de Dados
+mongoose.connect('mongodb://nodebr-user:node123456@ds261302.mlab.com:61302/nodedb',{useNewUrlParser:true});
+
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');

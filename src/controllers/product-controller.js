3314fc0087e2531330fa6d6e77be478ca2cdb273
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-18 03:56:39 -0300

    Validando e Salvando Produtos no banco de dados

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,10 +4,20 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 exports.post = (req, res, next) => {
-    var product = new Product();
-    product.title = req.body.title;
-    product.save();
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+
+    product
+        .save()
+        .then(x =>{
+            res.status(201).send({
+                message: 'Produto Cadastrado com Sucesso!'
+            });
+        }).catch(e =>{
+            res.status(400).send({
+                message: 'Falha ao Cadastrar o Produto',
+                data: e
+            });
+        });
 };
 
 exports.put = (req, res, next) =>{

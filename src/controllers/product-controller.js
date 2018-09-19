a8e1275d189832c3c78b48427421cfd92c02e50f
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-19 01:56:48 -0300

    findByIdAndUpdate - atualizando produto - controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -49,26 +49,6 @@ exports.getByTag = (req, res, next) =>{
     });
 };
 
-exports.put = (req, res, next) =>{
-    Product
-    .findByIdAndUpdate(req.params.id,{
-        $set:{
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price
-        }
-    }).then(x =>{
-        res.status(200).send({
-            message: 'Produto atualizado com sucesso!'
-        });
-    }).catch(e =>{
-        res.status(400).send({
-            message: 'Falha ao atualizar o produto',
-            data: e
-        });
-    });
-};
-
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
 
@@ -87,10 +67,23 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) =>{
-    const id = req.params.id;
-    res.status(201).send({
-        id:id,
-        item: req.body
+    Product
+    .findByIdAndUpdate(req.params.id,{
+        $set:{
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    }).then(x =>{
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e =>{
+        res.status(400).send({
+            message: 'Falha ao atualizar o produto',
+            data: e
+        });
     });
 };
 

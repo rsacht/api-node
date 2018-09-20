70cb9d2b0f815c975b6deb8d0ef6fa928a6044c0
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-20 19:56:49 -0300

    Criando o model Order

---
+++ src/models/order.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+    number: {
+        type: String,
+        required: true
+    },
+    createDate: {
+        type: Date,
+        required: true,
+        dafault: Date.now
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['created', 'done'],
+        dafault: 'created'
+    },
+    items: [{
+        quantity: {
+            type: number,
+            required: true,
+            default: 1
+        },
+        price:{
+            type: number,
+            required: true
+        },
+        product:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    }],
+});
+
+module.exports = mongoose.model('Order', schema);

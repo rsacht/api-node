1d2dba8402a32827ae6231c7cb62773137e7317d
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-21 04:10:07 -0300

    Criando arquivo de configuração global

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb://nodebr-user:node123456@ds261302.mlab.com:61302/nodedb',
+    sendgridKey: 'TBD',
+    conteinerConnectioString: 'TBD'
+}

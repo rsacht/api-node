5b960709a9816fdbd331f3d4fa425747febfe613
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-21 04:12:40 -0300

    Importando e Utilizando arquivo de configuração para pegar a string de configuração do Banco de Dados

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 const app = express();
 const router = express.Router();
 
 //Conexão com o Banco de Dados
-mongoose.connect('mongodb://nodebr-user:node123456@ds261302.mlab.com:61302/nodedb',{useNewUrlParser:true});
+mongoose.connect(config.connectionString,{useNewUrlParser:true});
 mongoose.set('useCreateIndex', true);
 
 //Carrega os Models

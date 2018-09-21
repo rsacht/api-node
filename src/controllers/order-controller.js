183c8dc2e71f83a5633a0e0f9209bdfb8b1c80cc
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-21 03:03:48 -0300

    Criando Controller do Order

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -3,6 +3,16 @@
 const repository = require('../repositories/order-repository');
 const uuid = require('node-uuid');
 
+exports.get = async(req, res, next) =>{
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }   
+}
 exports.post = async (req, res, next) => {
     try {
         await repository.create({

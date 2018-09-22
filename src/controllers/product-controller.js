c9f882611f9a9cbfedb871654dbf5888b3f870cc
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-22 03:01:03 -0300

    Configurando o envio e salvamento da imagem no Blob do Azure

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,7 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 const azure = require('azure-storage');
+const guid = require('guid');
 
 exports.get = async(req, res, next) =>{
     try {
@@ -62,7 +63,30 @@ exports.post = async (req, res, next) => {
     try{
         //Cria o Blob Service
         const blobSvc = azure.createBlobService(config.userImagesBlobConnectionString);
-        
+
+        //Cria uma string diferenciada para cada arquivo para evitar substituição
+        let filename = guid.raw().toString()+'.jpg';
+        //Converte Imagem em base64 
+        let rawdata = req.body.image;
+        //Remove os itens indesejados do cabeçalho
+        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        //Tipo fica no split 1
+        let type = matches[1];
+        //Cria um novo buffer da imagem para envio
+        let buffer = new Buffer(matches[2], 'base64');
+
+        //Salva a Imagem
+        //Criando o arquivo em formato de texto (base64) no conteiner product-images, com
+        // nome = filename, utilizano o buffer
+        await blobSvc.createBlockBlobFromText('product-images', filename, buffer,{
+            contentType: type
+        }, function(error, result, response){
+            if(error){
+                //Se der erro coloca essa imagem padrão
+                filename = 'default-product.png'
+            }
+        });
+
         await repository.create(req.body);
         res.status(201).send({
             message: 'Produto cadastrado com sucesso!'
